928ca7257fb594761c0d5e785d9f2b4dacacddf8
vahiidd <vahidkosari28@gmail.com>
2026-03-05 15:59:02 +0100

    Add User and TaskItem classes

---
+++ Entities/TaskItem.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlowApi.Entities;
+
+public enum TaskStatus
+{
+    Open = 0,
+    InProgress = 1,
+    Done = 2
+}
+
+public class TaskItem
+{
+    public int Id { get; set; }
+
+    [Required, MaxLength(200)]
+    public string Title { get; set; } = default!;
+
+    [MaxLength(4000)]
+    public string? Description { get; set; }
+
+    public TaskStatus Status { get; set; } = TaskStatus.Open;
+    public DateTime? DueDate { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdateAt { get; set; }
+
+    public int UserId { get; set; }
+    public User User { get; set; } = default!;
+
+}
---
+++ Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskFlowApi.Entities;
+
+[Index()]
+public class User
+{
+    public int id { get; set; }
+
+    [Required, MaxLength(256)]
+    public string Email { get; set; } = default!;
+
+    [Required, MaxLength(100)]
+    public string FullName { get; set; } = default!;
+
+    [Required]
+    public string PasswordHash { get; set; } = default!;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+}
+
